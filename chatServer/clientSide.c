5cfa51af0f4643c9f4803a1ffe3f12730f54705a
asimpso2 <allenrsimpson@gmail.com>
2017-03-14 15:56:01 -0700

    chatserver1

--- chatServer/clientSide.c
+++ chatServer/clientSide.c
@@ -61,55 +61,44 @@ int main(int argc, char **argv) {
     char command[1];
     char option[20];
     char size[8];
-    char content[256];
-    char packet[283]; //the packet size will be the command + option + size + content (which will be the MAX size of a file).
+    char content[2262155];
+    char packet[262173]; //the packet size will be the command + option + size + content (which will be the MAX size of a file).
 
     //NULL OUT FIELDS
-    int i = 0;
-    command[i] = '\0';
-    for (; i < sizeof(option); i++)
-        option[i] = '\0';
-    for (; i < sizeof(size); i++)
-        size[i] = '\0';
-    for (; i < sizeof(content); i++)
-        content[i] = '\0';
-    for (; i < sizeof(packet); i++)
-        packet[i] = '\0';
+//    int i = 0;
+//    command[i] = '\0';
+//    for (; i < sizeof(option); i++)
+//        option[i] = '\0';
+//    for (; i < sizeof(size); i++)
+//        size[i] = '\0';
+//    for (; i < sizeof(content); i++)
+//        content[i] = '\0';
+//    for (; i < sizeof(packet); i++)
+//        packet[i] = '\0';
 
-    //COPY DATA INTO FIELDS
-    //Having parsed your user input: "/w starkiller45 This is my first sent message!\n"
-    //into the correct parts: "w", "starkiller45", "This is my first sent message!\n"
-    //and having retreived the sizeof("This is my first sent message!\n")
-    //copy the data into the fields.
+//    //COMMAND
+//    command[0] = 'w'; //the command char parsed out of the user input
+//    //OPTION
+//    snprintf(option, 21, "%s", "starkiller45"); //the option parsed out of the user input
+//    //SIZE
+//    snprintf(size, 9, "%d", 100000); //the sizeof(the remaining message the user sent)
+//    //CONTENT
+//    snprintf(content, 257, "%s", "This is my first sent message!\n"); //the message
 
-    //COMMAND
-    command[0] = 'w'; //the command char parsed out of the user input
-    //OPTION
-    snprintf(option, 21, "%s", "starkiller45"); //the option parsed out of the user input
-    //SIZE
-    snprintf(size, 9, "%d", 100000); //the sizeof(the remaining message the user sent)
-    //CONTENT
-    snprintf(content, 257, "%s", "This is my first sent message!\n"); //the message
 
-    //use of snprintf(destination, # of bytes to print (copy), type of data, data):
-    //NOTE that the # of bytes to print should be 1 more than your buffer
-    //because the method adds a null terminating byte to the end (which could overwrite data).
-    //Here I'm just copying the data into the fields.
-
-    //COPY FIELDS INTO PACKET
-    int x;
-    //COMMAND -> packet[0]
-    packet[0] = command[0];
-
-    //OPTION -> packet[1 - 20]
-    for (x = 1; x < 21; x++)
-        packet[x] = option[x - 1];
-    //SIZE -> packet[21 - 29]
-    for (x = 21; x < 29; x++)
-        packet[x] = size[x - 21];
-    //CONTENT -> packet[29 - 284]
-    for (x = 29; x < 284; x++)
-        packet[x] = content[x - 29];
+//    int x;
+//    //COMMAND -> packet[0]
+//    packet[0] = command[0];
+//
+//    //OPTION -> packet[1 - 20]
+//    for (x = 1; x < 21; x++)
+//        packet[x] = option[x - 1];
+//    //SIZE -> packet[21 - 29]
+//    for (x = 21; x < 29; x++)
+//        packet[x] = size[x - 21];
+//    //CONTENT -> packet[29 - 284]
+//    for (x = 29; x < 284; x++)
+//        packet[x] = content[x - 29];
 
     //SHOW PACKET CONTENTS
     //printf("\n");
@@ -119,7 +108,7 @@ int main(int argc, char **argv) {
     //are represented here by the '*' character, and the \n character
     //will print out a new line.
 
-    write(sockfd, packet, 283);
+    //write(sockfd, packet, 283);
     //NOTE: printing out the packet as a STRING will result in: wstarkiller45
     //because the null bytes '\0' filling the rest of the OPTION field will terminate the string.
 
@@ -144,90 +133,96 @@ int main(int argc, char **argv) {
             packet[i] = '\0';
 
 
+        char writes[262155];
+        fgets(writes, 262155, stdin);
 
-
-
-        char writes[1000], send[1000];
-        fgets(writes, 999, stdin);
-
-
-        puts(writes);
-        if (writes[0] == '/')
+        if (writes[0] == '/') {
             switch (writes[1]) {
                 case 'b'    :
-                    packet[0] = 'b';
-                    for (x = 1; x < sizeof(writes)-2; x++)
-                        content[x] = writes[x+2];
-
+                    command[0] = 'b';
                     break;
 
                 case 'c'    :
-                    packet[0] = 'c';//command list//commands.commandList(i);break;
+                    command[0] = 'c';//command list//commands.commandList(i);break;
                     break;
 
                 case 'd'    :
-                    packet[0] = 'd';//commands.disconnect(i);
+                    command[0] = 'd';//commands.disconnect(i);
                     break;
 
                 case 'e'    :
-                    packet[0] = 'e';//commands.exitRoom(i);
+                    command[0] = 'e';//commands.exitRoom(i);
                     break;
 
                 case 'f'    :
-                    packet[0] = 'f';//commands.pFile(clinetList,packet);private file
+                    command[0] = 'f';//commands.pFile(clinetList,packet);private file
                     break;
 
                 case 'g'   :
-                    packet[0] = 'g';//group file
+                    command[0] = 'g';//group file
                     break;
                 case 'h'    :
-                    packet[0] = 'h';//who's in my room?
+                    command[0] = 'h';//who's in my room?
                     break;
 
                 case 'l'    :
-                    packet[0] = 'l';//list all users in all rooms
+                    command[0] = 'l';//list all users in all rooms
                     break;
 
                 case 'n'    :
-                    packet[0] = 'n';//name registration
+                    command[0] = 'n';//name registration
                     break;
 
                 case 'r'    :
-                    packet[0] = 'r';//explicitly send message to room
+                    command[0] = 'r';
+                    for (x = 1; x < sizeof(writes) - 2; x++)
+                        content[x] = writes[x + 2];//explicitly send message to room
                     break;
 
                 case 's'    :
-                    packet[0] = 's';//switch rooms
+                    command[0] = 's';//switch rooms
                     break;
 
                 case 'w'    :
-                    packet[0] = 'w';//Whisper
+                    command[0] = 'w';//Whisper
                     break;
 
                 default        :
                     break;
-            }//dsfajkljdslkajflkdsa
-        else
-        {
+            }
+        }
+        else {
+            int u;
+            puts("else");
+            command[0] = 'r';
+            puts("filled command");
+            for (u = 0; u < sizeof(writes); u++) {
+               // printf("%d\n", u);
+                content[u] = writes[u];
+            }
+            puts("filled content");
+            //puts(packet);
+        }
 
-                    packet[0] = 'b';
-            for (x = 0; x < sizeof(writes); x++)
-                content[x] = writes[x];
-    }
+        puts("end else");
 
-
+        //content 262144
+        packet[0] = command[0];
         for (x = 1; x < 21; x++)
             packet[x] = option[x - 1];
         //SIZE -> packet[21 - 29]
         for (x = 21; x < 29; x++)
             packet[x] = size[x - 21];
         //CONTENT -> packet[29 - 284]
-        for (x = 29; x < 284; x++)
+        for (x = 29; x < 262115; x++)
             packet[x] = content[x - 29];
 
-
-       puts(packet);
-        write(sockfd, packet, strlen(packet));
+        puts("PACKET");
+        for(x = 0; x<1000; x++)
+        {
+            printf("%c", packet[x]);
+        }
+        // write(sockfd, packet, strlen(packet));
     }
 
 }//end main
